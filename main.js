df06d908ab0fc4a0b3e34eb861126230624c476a
lionanubhav <125356613+lionanubhav@users.noreply.github.com>
2023-10-27 15:22:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+random_number=" ";
+timer_counter=" 0 ";
+timer_check=" ";
+drawn_sketch=" ";
+score=" 0 ";
+
+quick_draw_data_set=["sketch1.png"]
+random_number = Math.floor((Math.random()*array_1.length)+1)
+
